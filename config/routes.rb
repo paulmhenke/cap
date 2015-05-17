36541e3694bd74de1b2062bb51a7cb5989c75711
paulhenke <paulmhenke@gmail.com>
2015-05-17 18:21:28 +0000

    installed devise, no flash messages yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
 
   get 'home/about'
+  
+  root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
